90ccf8e1aab312c5316b36ad6311ebf99f97dbd3
jhLee <nryn6563@kakao.com>
2023-01-26 07:21:55 +0900

    닫기버튼 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,26 @@ module.exports = {
     backgroundPosition: {
       left : 'left',
       'left-mid': 'left 50%',
+    },
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
     }
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
   ],
   darkMode: 'class',
 }
